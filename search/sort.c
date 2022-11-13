092793bb1caf2ac33f2a703c250cb7b6abca6285
Pallavi <101661582+EscCoder@users.noreply.github.com>
2022-11-13 10:44:29 +0530

    v2

--- search/sort.c
+++ search/sort.c
@@ -1,11 +1,13 @@
 //WAP sort an array
 
-// Online C compiler to run C program online
+
 #include <stdio.h>
 
 void main() 
 {
+
     int arr[9]={60,13,17,18,11,20,21,15,16};
+    int swap=0;
     int i,temp,j;
     printf("Before sorting array is \n");
     for(i=0;i<9;i++)
@@ -14,18 +16,21 @@ void main()
     }
     
     
-    for (j=0;j<9;j++)
-    {
+    do{
+        swap=0;
         for (i=0;i<9;i++)
         {
             if (arr[i]>arr[i+1])
             {
+                swap=swap+1;
                 temp=arr[i];
                 arr[i]=arr[i+1];
                 arr[i+1]=temp;
             }
+        
         }
-    }
+       // printf("\n swap is %d",swap);
+    }while(swap!=0);
     
     printf("\nAfter sorting array is\n");
     for(i=0;i<9;i++)
