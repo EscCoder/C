52948b778cd464ab516620971f66bb996244c6eb
Pallavi <101661582+EscCoder@users.noreply.github.com>
2022-12-01 14:58:50 +0530

    v1.0

---
+++ dy_m_alloc/malloc.c
@@ -0,0 +1,35 @@
+//To uderstand the use of malloc pre defined function
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() 
+{
+    int i,n;
+    int *ptr;
+    
+    printf("This is to demonstrate the use of malloc() function, \na predefined function used in dynamic memory allocation:\n");
+    printf("\tEnter the number of intergers :\n");
+    scanf("%d",&n);
+    printf("Now we will create a dynamic memory location as per our requirement of %d\n",n);
+    
+    ptr = (int* )malloc(n*sizeof(int));      //size of integer offered by the compiler
+    // type cast the void pointer returned by malloc
+    
+    if (ptr==NULL)
+    {
+        printf("No memory space left :");
+       
+    }
+    
+    for (i=0;i<n;i++)
+    {
+        printf("Enter integer - %d->",i+1);                //ptr is pointing to base address
+        scanf("%d",ptr+i);                                // progress to next memory block 
+    }
+    
+    for (i=0;i<n;i++)
+    {
+        printf("%d\t",*(ptr+i));                          //print the content at (ptr+i)^th memory location
+    }
+}
